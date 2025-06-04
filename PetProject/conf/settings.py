3f35503236c86a5763c86b84e554635d0f708391
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2025-06-04 12:45:29 -0400

    [IMP] automatic detection of .env file

--- PetProject/conf/settings.py
+++ PetProject/conf/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+env_path = Path(__file__).resolve().parent.parent.parent / 'Docker/.env'
+print(env_path)
+load_dotenv(dotenv_path=env_path)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
