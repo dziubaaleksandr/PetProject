ac4f80a56d5f875ee734d83d9d1ea44971880f6b
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2025-06-21 12:59:56 -0400

    [FEAT] analytics: Add a Transaction model

--- PetProject/analytics/models.py
+++ PetProject/analytics/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Transaction(models.Model):
+    TRANSACTION_TYPES = [
+        ('income', 'Income'),
+        ('expense', 'Expense')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPES)
+
+    def __str__(self):
+        return f"{self.date} - {self.description} - {self.amount}"
