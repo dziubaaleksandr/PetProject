db0994a2f918781b6cb5d1b9e8651308506babe4
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2025-06-21 13:00:56 -0400

    [FEAT] analytics: Add a Category model

--- PetProject/analytics/models.py
+++ PetProject/analytics/models.py
@@ -15,3 +15,11 @@ class Transaction(models.Model):
 
     def __str__(self):
         return f"{self.date} - {self.description} - {self.amount}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
