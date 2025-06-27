0c28f0504189ca9d3aaa91fd431d636cc6c0301d
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2025-06-27 15:50:15 -0400

    [FEAT] analytics: Add url for displaying a dashboard

--- PetProject/analytics/urls.py
+++ PetProject/analytics/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UploadView
+from .views import UploadView, DashboardView
 
 urlpatterns = [
     path('upload/', UploadView.as_view(), name='upload'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
