77a01575b2f9f3e289ce70319ee1b48717c9b7f9
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2025-07-03 12:01:13 -0400

    [FEAT] analytics: Set up links for seeing insights

--- PetProject/analytics/urls.py
+++ PetProject/analytics/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UploadView, DashboardView
+from .views import UploadView, DashboardView, InsightView
 
 urlpatterns = [
     path('upload/', UploadView.as_view(), name='upload'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
+    path('insights/', InsightView.as_view(), name='insights'),
 ]
