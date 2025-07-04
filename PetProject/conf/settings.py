aa9de39f27eb58ad444e2fb5f81084bb29db2054
Oleksandr Dziuba <dziubaaleksandr22@gmail.com>
2025-07-04 12:41:13 -0400

    [FEAT] conf: Add a path to global templates

--- PetProject/conf/settings.py
+++ PetProject/conf/settings.py
@@ -34,19 +34,19 @@ ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'upload'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = 'logout'
 
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
+    'analytics',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
-    'analytics',
 ]
 
 MIDDLEWARE = [
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'conf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
